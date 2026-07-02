aeece700c5e765f23bae13fcf7844e008eafbb25
pablo50x20 <pablo50x20@gmail.com>
2026-07-02 16:59:11 -0400

    arregla error en sincronización página web

--- game.js
+++ game.js
@@ -777,4 +777,5 @@ function showToast(msg) {
   const t = document.getElementById('toast');
   t.textContent = msg; t.classList.add('show');
   setTimeout(() => t.classList.remove('show'), 2500);
+  
 }
